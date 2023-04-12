be8cac9120b213b89ef3690d227a1c1fb180e092
FrancescaLoria <francesca.loria90@hotmail.it>
2023-04-12 19:24:22 +0200

    bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      myImg: "img.jpg",
+    };
+  },
+  methods: {
+    getImgUrl() {
+      return new URL(`../assets/${this.myImg}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <img :src="getImgUrl()" alt="adventure-time" />
+  </div>
+</template>
+
+<style></style>
